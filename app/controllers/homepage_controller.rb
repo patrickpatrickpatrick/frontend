e6c4384a40042d84be4c668c0fde5979a60a0a65
Patrick Cartlidge <patrick.cartlidge@digital.cabinet-office.gov.uk>
2022-06-16 11:34:45 +0100

    Use new homepage gem layout on homepage
    As part of the work removing emergency banner from static,
    a new gem layout has been created in static for homepage.
    This is because the emergency banner has specific styling
    when it is present on the homepage.
    
    Previously, the emergency banner homepage styles were applied
    by using a parent class selector but this is against the
    isolation principle of components. So the emergency banner now
    uses a parameter which when set to true applies the correct
    styling. This meant that a new gem layout needed to be created
    for homepage with this parameter set for the emergency banner
    (when the emergency banner is displayed).

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,10 +1,10 @@
 class HomepageController < ContentItemsController
   include Cacheable
-  slimmer_template "gem_layout_full_width"
+  slimmer_template "gem_layout_homepage"
 
   def index
     set_slimmer_headers(
-      template: "gem_layout_full_width",
+      template: "gem_layout_homepage",
       remove_search: true,
     )
   end
